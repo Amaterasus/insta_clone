a2be5eb455bffcd6337edd2ea70b924e80ce0b37
dinnosaur <dinnoroni@gmail.com>
2020-03-24 16:45:40 +0000

    Typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     
    #followers and followee relationship 
     has_many :active_follows, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy
-    has_many :folowees, through: :active_follows, source: :followee
+    has_many :followees, through: :active_follows, source: :followee
     has_many :passive_follows, class_name: "Follow", foreign_key: :followee_id, dependent: :destroy
     has_many :followers, through: :passive_follows, source: :follower
 
