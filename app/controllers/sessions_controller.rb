2e0fa4c1813b68a7547290cedac08519422013de
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-24 15:43:36 +0000

    Added login stuff - NZ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+
+    def login
+    end
+
+    def signup
+    end
+
+    def logout
+        # Changes session to nil i.e. logging out
+        session[:user_id] = nil
+        redirect_to root_path
+    end
+
+    def login_verify
+        # Finds user by matching usernames passed in
+        # Authenticates it with the password provided
+        # Either says failed or goes to their 'home' page (and starts the session)
+        @user = User.find_by(user_name: params[:user][:user_name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            # Will put my followers posts path or soemthing like that
+            redirect_to BLANK
+        else
+            flash[:errors] ||= []
+            flash[:errors] << "Username or Password is incorrect. Please Try Again." 
+            redirect_to login_path
+        end
+    end
+
 end
