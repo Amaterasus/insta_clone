95d2e5c8354a7769fe400724543bb33b873b7198
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-24 16:28:14 +0000

    added login to routes - NZ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#login_verify"
+  post "/logout", to: "sessions#logout"
 end
