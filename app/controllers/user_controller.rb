046dd67604a69dad4c13fe88f45d44422cf34e58
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-24 15:20:22 +0000

    user controller created - NZ

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+
+    def create
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:user_name, :bio)
+    end
+
+end
