a1c27708236d9e1578d670042733ed1a1e0024d7
dinnosaur <dinnoroni@gmail.com>
2020-03-24 16:40:18 +0000

    Association and validation(user)

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,7 @@
 class Follow < ApplicationRecord
+    #self join
+    belongs_to :follower, class_name: "User", foreign_key: :follower_id
+    belongs_to :followee, class_name: "User", foreign_key: :followee_id
+
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,5 @@
 class Like < ApplicationRecord
+    belongs_to :user 
+    belongs_to :post 
+    #need to validate current_user is not the creator of posts
 end
