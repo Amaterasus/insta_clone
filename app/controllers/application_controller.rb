87efdae283916e7ddab8fde4512b249e761d89c3
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-24 15:53:46 +0000

    Added logged in validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+    def current_user
+        # In sessions updates the user to be this user
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        else
+        end
+    end
+
+    def logged_in?
+        # Converts current user to boolean
+        !!current_user
+    end
+
+    def authorized
+        # If allowed to be on site
+        redirect_to login_path unless logged_in?
+    end
+
 end
