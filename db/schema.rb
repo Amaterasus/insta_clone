4074864fad180eefe0fd0e89629c977f11814bb2
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-24 16:28:45 +0000

    added passwords to users and admin functional - NZ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_095455) do
+ActiveRecord::Schema.define(version: 2020_03_24_161800) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "followee_id"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_03_24_095455) do
     t.string "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
