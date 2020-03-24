8e49a9c9104b37c0ad0ddec856eae1929208acec
Amaterasus <skyetay03@googlemail.com>
2020-03-24 10:12:34 +0000

    fixed migration issue

--- db/migrate/20200324095455_create_follows.rb
+++ db/migrate/20200324095455_create_follows.rb
@@ -1,7 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[6.0]
   def change
     create_table :follows do |t|
-      t.integerfollowee_id :follower_id
+      t.integer :followee_id 
+      t.integer :follower_id
 
       t.timestamps
     end
