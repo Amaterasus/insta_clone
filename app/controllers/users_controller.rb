94d5af1639b3bbb03922defeba85231293319c2e
dinnosaur <dinnoroni@gmail.com>
2020-03-25 11:35:33 +0000

    User.show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
-    before_action :authorized, only: [:index, :show, :edit, :update, :destroy]
+    before_action :authorized, only: [:index, :edit, :update, :destroy]
+    before_action :find_user, only: [:show]
 
     def create
         @user = User.create user_params
+    end
+
+    def show 
+        
     end
 
     def destroy
@@ -18,4 +23,5 @@ class UsersController < ApplicationController
         params.require(:user).permit(:user_name, :password, :password_confirmation, :bio)
     end
 
+    
 end
