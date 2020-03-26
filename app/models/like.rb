20de74e24b1a44b93b41f646e29061b54dc1d9a2
Amaterasus <skyetay03@googlemail.com>
2020-03-26 11:17:14 +0000

    users can now comment on posts

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-    belongs_to :user 
-    belongs_to :post 
-    #need to validate current_user is not the creator of posts
+  belongs_to :user
+  belongs_to :post
+  #need to validate current_user is not the creator of posts
 end
