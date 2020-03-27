7b7806df98b1efc58ae2afbedc3096bb6001705f
Amaterasus <skyetay03@googlemail.com>
2020-03-27 02:27:38 +0000

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "net/http"
+
 User.destroy_all
 Post.destroy_all
 Like.destroy_all
@@ -5,8 +7,10 @@ Follow.destroy_all
 Comment.destroy_all
 Faker::UniqueGenerator.clear
 
-nick = User.create(user_name: "nick", bio: "Greek Man", password: "cat")
+start_time = Time.now
+
 skye = User.create(user_name: "skye", bio: "Overachieving coder", password: "cat")
+nick = User.create(user_name: "nick", bio: "Greek Man", password: "cat")
 dinno = User.create(user_name: "dinno", bio: "Man in lockdown", password: "cat")
 
 Follow.create(follower: nick, followee: skye)
@@ -16,59 +20,67 @@ Follow.create(follower: skye, followee: dinno)
 Follow.create(follower: dinno, followee: nick)
 Follow.create(follower: dinno, followee: skye)
 
-
 # User Seeds
-250.times do 
-    user = User.create(user_name: Faker::Name.unique.first_name.downcase, bio: Faker::Movies::StarWars.quote, password: "cat")
-    puts "#{user.user_name} created"
+250.times do
+  user = User.create(user_name: Faker::Name.unique.first_name.downcase, bio: Faker::Movies::StarWars.quote, password: "cat")
+  puts "#{user.user_name} created"
 end
 
-user_ids = User.all.map{ |user| user.id }
-
+user_ids = User.all.map { |user| user.id }
 
 # Posts seeds
 5000.times do
-    post = Post.create(title: Faker::Quote.famous_last_words, image: "https://i.picsum.photos/id/#{(1..1084).to_a.sample}/300/300", user_id: user_ids.sample)
+  while true
+    url = URI.parse("https://i.picsum.photos/id/#{(1..1084).to_a.sample}/300/300.jpg")
+    req = Net::HTTP.new(url.host, url.port)
+    req.use_ssl = true
+    res = req.request_head(url.path)
+    if res.code == "200"
+      post = Post.create(title: Faker::Quote.famous_last_words, image: url, user_id: user_ids.sample)
+      puts "#{Post.count} posts created"
+      break
+    else
+      puts "ERROR 404 LOOK AGAIN!!!"
+    end
+  end
 end
-  
-puts "#{Post.count} posts created"
 
-post_ids = Post.all.map{ |post| post.id }
+post_ids = Post.all.map { |post| post.id }
 
 # Follow seeds
 
 User.all.each do |u|
-    x = []
-    100.times do 
-        x << user_ids.sample
-    end
-    if x.include?(u.id)
-        x.delete(u.id)
-    end
-    x.uniq.each do |user|
-        Follow.create(follower_id: user, followee_id: u.id)
-    end
+  x = []
+  100.times do
+    x << user_ids.sample
+  end
+  if x.include?(u.id)
+    x.delete(u.id)
+  end
+  x.uniq.each do |user|
+    Follow.create(follower_id: user, followee_id: u.id)
+    puts "#{Follow.count} follows created"
+  end
 end
-
-puts "#{Follow.count} follows created"
 
 # Like seeds
 
 Post.all.each do |post|
-    5.times do
-        x = post.user.followers.sample
-        if Like.where(post: post, user: x).size == 0
-            like = Like.create(post: post, user: x)
-        end
+  5.times do
+    x = post.user.followers.sample
+    if Like.where(post: post, user: x).size == 0
+      like = Like.create(post: post, user: x)
     end
+    puts "#{Like.count} likes created"
+  end
 end
-
-puts "#{Like.count} likes created"
 
 # Comment seeds
 
 15000.times do
-    comment = Comment.create(user_id: user_ids.sample, post_id: post_ids.sample, content: Faker::Movie.quote)
+  comment = Comment.create(user_id: user_ids.sample, post_id: post_ids.sample, content: Faker::Movie.quote)
+  puts "#{Comment.count} comments created"
 end
 
-puts "#{Comment.count} comments created"
+end_time = Time.now
+puts "total time taken #{(end_time - start_time).round(2)} seconds"
